08bbb1e2fc75f7a1fdf4d9ecbbbe37535299eb45
Anderson Toshiyuki Sasaki <ansasaki@redhat.com>
2024-11-20 18:03:14 +0100

    Fix IP address setting for the verifier and registrar
    The IP addresses should be set as 0.0.0.0 to accept any incoming
    requests.
    
    Signed-off-by: Anderson Toshiyuki Sasaki <ansasaki@redhat.com>

--- start-keylime-registrar.sh
+++ start-keylime-registrar.sh
@@ -1,1 +1,6 @@
-podman run --rm -d -v ~/keylime/CERTS:/var/lib/keylime:z -p 8890:8890 -p 8891:8891 --network=host  localhost/keylime_registrar
+podman run --rm -d \
+    -v ~/keylime/CERTS:/var/lib/keylime:z \
+    -p 8890:8890 -p 8891:8891 \
+    -e KEYLIME_REGISTRAR_IP=0.0.0.0 \
+    --name registrar \
+    localhost/keylime_registrar
--- start-keylime-verifier.sh
+++ start-keylime-verifier.sh
@@ -1,2 +1,8 @@
 mkdir -p ~/keylime/CERTS
-podman run --rm -d -v ~/keylime/CERTS:/var/lib/keylime:z -p 8880:8880 -p 8881:8881 --network=host localhost/keylime_verifier
+podman run --rm -di \
+    -v ~/keylime/CERTS:/var/lib/keylime:z \
+    -p 8880:8880 \
+    -p 8881:8881 \
+    --name verifier \
+    -e KEYLIME_VERIFIER_IP=0.0.0.0 \
+    localhost/keylime_verifier
